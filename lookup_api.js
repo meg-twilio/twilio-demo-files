e45e4e4c8961af05cf1976a6b4b70869addb29cf
meg-twilio <mcummings@twilio.com>
2020-03-13 09:49:22 -0700

    added phone number lookups

--- lookup_api.js
+++ lookup_api.js
@@ -1,3 +1,11 @@
 const twilio = require('twilio')
 const client = new twilio(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN)
  
+
+client.lookups.phoneNumbers('+19045371699')
+    .fetch({type: ['carrier']})
+    .then(response => {
+        console.log(response.carrier.name)
+        // console.log(response)    
+    }
+    );
